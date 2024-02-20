74ecfa1839ee6f8270b797054f7b9f9109b1ec4c
Grandelius <jonatan.grandelius@iths.se>
2024-02-20 17:50:02 +0100

    Implemented NHLApis dependency injection extension

---
+++ NHLStatistics/Domain/PlayerDataSearchResult.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+
+namespace NHLStatistics.Domain
+{
+    public class PlayerDataSearchResult
+    {
+        //
+        // Summary:
+        //     The unique identifier for the NHL player
+        //     Example: 8478402 - Connor McDavid
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        //
+        // Summary:
+        //     The current team identifier for the NHL player
+        //     Example: 14 - Tampa Bay Lightning
+        [JsonProperty("currentTeamId")]
+        public int? CurrentTeamId { get; set; }
+
+        //
+        // Summary:
+        //     The first name of the NHL player
+        //     Example: Connor
+        [JsonProperty("firstName")]
+        public string FirstName { get; set; }
+
+        //
+        // Summary:
+        //     The full name of the NHL player
+        //     Example: Connor McDavid
+        [JsonProperty("fullName")]
+        public string FullName { get; set; }
+
+        //
+        // Summary:
+        //     The last name of the NHL player
+        //     Example: McDavid
+        [JsonProperty("lastName")]
+        public string LastName { get; set; }
+
+        //
+        // Summary:
+        //     The position code of the NHL player
+        //     Excample: C - Center
+        [JsonProperty("positionCode")]
+        public string PositionCode { get; set; }
+
+        //
+        // Summary:
+        //     The sweater number of the NHL player
+        //     Example: 97 - Connor McDavid
+        [JsonProperty("sweaterNumber")]
+        public int? SweaterNumber { get; set; }
+    }
+
+}
--- NHLStatistics/Program.cs
+++ NHLStatistics/Program.cs
@@ -1,3 +1,7 @@
+using Nhl.Api;
+using Nhl.Api.Extensions.Microsoft.DependencyInjection;
+using Nhl.Api.Models.Player;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddNhlApi();
+//builder.Services.AddTransient<PlayerDataSearchResult>();
+//builder.Services.AddSingleton<INhlPlayerApi, NhlPlayerApi>();
+//builder.Services.AddSingleton<INhlLeagueApi, NhlLeagueApi>();
+//builder.Services.AddSingleton<INhlStatisticsApi, NhlStatisticsApi>();
 
 var app = builder.Build();
 
