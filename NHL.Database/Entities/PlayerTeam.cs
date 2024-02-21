4b410edacca1da274950f4cb4641072dda76abfb
Grandelius <jonatan.grandelius@iths.se>
2024-02-21 16:52:56 +0100

    implemented entities and interfaces for database tables

---
+++ NHL.Database/Entities/PlayerTeam.cs
@@ -0,0 +1,14 @@
+﻿using NHL.Database.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHL.Database.Entities
+{
+    public class PlayerTeam : IEntity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ NHL.Database/Entities/Season.cs
@@ -0,0 +1,14 @@
+﻿using NHL.Database.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHL.Database.Entities
+{
+    internal class Season : IEntity
+    {
+        public int Id { get; set; }
+    }
+}
